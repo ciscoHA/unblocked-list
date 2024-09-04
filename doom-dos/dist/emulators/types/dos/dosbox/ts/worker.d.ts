aa5a2823e15000fc9a01a3677078e94e88e32007
ciscoHA <123529804+ciscoHA@users.noreply.github.com>
2024-09-04 21:03:01 +1000

    reinstate changes from pull request and removal of old doom-dos folder (now doom-js-dos)

--- doom-dos/dist/emulators/types/dos/dosbox/ts/worker.d.ts
+++
@@ -1,3 +0,0 @@
-import { WasmModule } from "../../../impl/modules";
-import { TransportLayer } from "../../../protocol/protocol";
-export declare function dosWorker(workerUrl: string, wasmModule: WasmModule, sessionId: string): Promise<TransportLayer>;
